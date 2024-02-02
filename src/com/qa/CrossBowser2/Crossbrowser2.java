26b47a72bbcdb4b5e0e43b16f9ddbb0f85985b57
dastagiri <mpdastagiri98@gmail.com>
2024-02-02 12:47:29 +0530

    this is commit from india time guy

--- src/com/qa/CrossBowser2/Crossbrowser2.java
+++ src/com/qa/CrossBowser2/Crossbrowser2.java
@@ -28,7 +28,10 @@ public class Crossbrowser2 {
     
     String browser = pro.getProperty("browser");
     System.out.println(browser);
-    
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+
     if (browser.equalsIgnoreCase("chrome")) 
     
     {
